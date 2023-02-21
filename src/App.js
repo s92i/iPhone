313c72162f78d26fa5c97deaf375799dcbf52f23
s92i <sofiane.92izii@gmail.com>
2023-02-22 00:26:13 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/GlobalStyle"
+import Quote from "./sections/Quote";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Quote />
+    </>
+  );
+}
+
+export default App;
